8019e97cd7342faefcc87cbb963d10ea323a7f41
djedwabsky <djedwabsky@gmail.com>
2025-10-24 05:13:21 +0300

    Added production check  for redirection in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -11,15 +11,22 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* <Provider store={store}><App /></Provider> */}
     <KindeProvider
-		clientId="e64b0f728627450894b07737548d7df8"
-		domain="https://davidjedwebdesign.kinde.com"
-		redirectUri="http://localhost:5173"
-		logoutUri="http://localhost:5173"
-	>
-    <TodosContextProvider>
-      <App />
-    </TodosContextProvider>
-  </KindeProvider>
-    
+      clientId="e64b0f728627450894b07737548d7df8"
+      domain="https://davidjedwebdesign.kinde.com"
+      redirectUri={
+        process.env.NODE_ENV === "production"
+          ? "https://my-todo-app-ochre-three.vercel.app"
+          : "http://localhost:5173"
+      }
+      logoutUri={
+        process.env.NODE_ENV === "production"
+          ? "https://my-todo-app-ochre-three.vercel.app"
+          : "http://localhost:5173"
+      }
+    >
+      <TodosContextProvider>
+        <App />
+      </TodosContextProvider>
+    </KindeProvider>
   </StrictMode>
 );
